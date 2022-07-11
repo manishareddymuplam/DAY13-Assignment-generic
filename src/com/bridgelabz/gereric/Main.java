754402f66e17fb9a668d6cf2d27aa90411d970de
manishareddymuplam <manishamuplam@gmail.com>
2022-07-12 00:54:17 +0530

    added:findmaximumint

---
+++ src/com/bridgelabz/gereric/Main.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.gereric;
+
+public class Main {
+	public static void main(String[] args) {
+        System.out.println("Welcome1 to generics");
+        //UC1 find maximum of three Integers
+        GenericClass genericClass = new GenericClass();
+        Integer printValue = genericClass.testMaxium(1,7,4);
+        System.out.println("The Maximum integer is : "+printValue);
+    }
+}
